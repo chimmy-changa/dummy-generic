488707d257b8569be61ab787c2060cdacf9e1ec3
Sourabh Kondapaka <skondapa@mathworks.com>
2021-08-27 11:25:26 +0530

    initial

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+
+tests_require = [
+    "pytest",
+    "pytest-env",
+    "pytest-cov",
+    "pytest-mock",
+    "pytest-dependency",
+    "pytest-aiohttp",
+    "requests",
+    "psutil",
+    "aioresponses",
+]
+
+setup(
+    name="dummy-generic",
+    version="0.1.0",
+    description="Testing installation of Package",
+    python_requires="~=3.7",
+    install_requires=["jupyter-server-proxy", "aiohttp>=3.7.4"],
+    setup_requires=["pytest-runner"],
+    tests_require=tests_require,
+    extras_require={"dev": ["aiohttp-devtools"] + tests_require},
+    url="#",
+    author="auth",
+    author_email="author@email.com",
+    license="MIT",
+    packages=["dummy_generic"],
+    entry_points={
+        "console_scripts": ["say_hi = dummy_generic.get:print_hello"],
+    },
+    zip_safe=False,
+)
---
+++ tests/test_get.py
@@ -0,0 +1,5 @@
+from dummy_generic.get import *
+
+
+def test_get_hello():
+    assert "hello" == get_hello()
